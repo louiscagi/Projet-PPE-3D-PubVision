6b4b81008c90d4c912f2c40c81390d57dbbe1f5e
Sebastien BARANGER <sebastienbaranger78@outlook.fr>
2024-04-10 16:43:44 +0200

    feat : Ajout de l'image demo

--- pubvision/app/js/script.js
+++ pubvision/app/js/script.js
@@ -59,6 +59,38 @@ const headerActive = function () {
     backTopBtn.classList.remove("active");
   }
 }
+window.addEventListener('scroll', headerActive);
 
+// Attend que le DOM soit complètement chargé
+document.addEventListener('DOMContentLoaded', () => {
+  // Ajoute un écouteur d'événements au bouton pour la récupération des données
+  const fetchDataButton = document.getElementById('fetch-data-button');
+  if (fetchDataButton) { // S'assure que le bouton existe avant d'ajouter l'écouteur d'événements
+    const resultsContainer = document.getElementById('results');
+
+    fetchDataButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Empêche le comportement par défaut du lien
+      fetch('/api/images') // Utilise le nouvel endpoint pour les données
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          // Efface les résultats précédents
+          resultsContainer.innerHTML = '';
+          // Crée une nouvelle balise image et la définit avec l'URL de l'image
+          const img = document.createElement('img');
+          img.src = data.imageData; // Utilise l'attribut imageData contenant l'image en base64
+          img.alt = 'Image chargée'; // Ajoutez un texte alternatif pour l'accessibilité
+          resultsContainer.appendChild(img);
+        })
+        .catch(error => {
+          console.error('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+        });
+    });
+  }
+});
 
 
